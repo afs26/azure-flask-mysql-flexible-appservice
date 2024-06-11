f89c2c462970358cb71d4fa45b0b636f69ee821e
AFS26 <afs26@C02D706DP3YV.local>
2024-06-11 15:40:35 -0400

    Refactor for testing

---
+++ flaskapp/config/production.py
@@ -0,0 +1,18 @@
+import os
+
+DEBUG = False
+
+
+if "WEBSITE_HOSTNAME" in os.environ:
+    ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]]
+else:
+    ALLOWED_HOSTS = []
+
+
+dbuser = os.environ["MYSQL_USER"]
+dbpass = os.environ["MYSQL_PASS"]
+dbhost = os.environ["MYSQL_HOST"]
+dbname = os.environ["MYSQL_DATABASE"]
+dbport = os.environ.get("MYSQL_PORT", 3306)
+
+DATABASE_URI = f"mysql+mysqlconnector://{dbuser}:{dbpass}@{dbhost}:{dbport}/{dbname}"
